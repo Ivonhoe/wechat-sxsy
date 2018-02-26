15463cacc2f5b9e1e2d05a17784ebcb5c266db60
杨帆 <yangfan@51xianqu.net>
2018-02-26 18:10:37 +0800

    修改首页

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //app.js
-const AV = require('./libs/av-weapp-min');
+const AV = require('/libs/av-weapp-min');
 
 // LeanCloud 应用的 ID 和 Key
 AV.init({
@@ -8,6 +8,10 @@ AV.init({
 });
 
 App({
+  data:{
+    AV:AV
+  },
+
   onLaunch: function () {
     // demo
     this.testStroage()
@@ -50,19 +54,29 @@ App({
 
   // leancloud demo
   testStroage: function(){
-    // 声明一个 Todo 类型
-    var Todo = AV.Object.extend('Todo');
-    // 新建一个 Todo 对象
-    var todo = new Todo();
-    todo.set('title', '工程师周会');
-    todo.set('content', '每周工程师会议，周一下午2点');
-    todo.set('test','test 工程师')
-    todo.save().then(function (todo) {
-      // 成功保存之后，执行其他逻辑.
-      console.log('New object created with objectId: ' + todo.id);
-    }, function (error) {
-      // 异常处理
-      console.error('Failed to create new object, with error message: ' + error.message);
-    });
+    // // 声明一个 Todo 类型
+    // var Todo = AV.Object.extend('Todo');
+    // // 新建一个 Todo 对象
+    // var todo = new Todo();
+    // todo.set('title', '工程师周会');
+    // todo.set('content', '每周工程师会议，周一下午2点');
+    // todo.set('test','test 工程师')
+    // todo.save().then(function (todo) {
+    //   // 成功保存之后，执行其他逻辑.
+    //   console.log('New object created with objectId: ' + todo.id);
+    // }, function (error) {
+    //   // 异常处理
+    //   console.error('Failed to create new object, with error message: ' + error.message);
+    // });
+
+    // new AV.Query('ShuXiangSuiYue')
+    //   .descending('createTime')
+    //   .find()
+    //   // .then(items => this.setData({ items }))
+    //   .then(function(todo){
+    //     console.log('New object created with objectId: ' + todo.length);
+    //   }, function(error){
+    //     console.error(error)
+    //   });
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,7 @@ const app = getApp()
 Page({
   data: {
     currentTab: 0,
-    items: [1,2,3,4,5,6,7],
+    items: [],
     hidden: false,
     userInfo: {},
     hasUserInfo: false,
@@ -13,22 +13,18 @@ Page({
     autoplay: false,
   },
 
-  swichNav: function (e) {
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      this.setData({
-        currentTab: e.target.dataset.current
-      })
-    }
-  },
-  bindChange: function (e) {
-    this.setData({ currentTab: e.detail.current });
+  onReady: function () {
+    new app.data.AV.Query('ShuXiangSuiYue')
+      .descending('createTime')
+      .find()
+      // .then(items => this.setData({ items }))
+      .then(items => this.setData({ items }))
+      .catch(console.error);;
   },
 
-/////////////////////////////////////demo
+  /////////////////////////////////////demo
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -39,7 +35,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -61,8 +57,8 @@ Page({
       })
     }
   },
-  
-  getUserInfo: function(e) {
+
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
